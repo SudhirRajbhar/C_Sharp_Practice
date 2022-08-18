84c03e0d697b12140866afe88c9382a9c8ae4c49
SudhirRajbhar <82689435+SudhirRajbhar@users.noreply.github.com>
2022-08-18 16:06:58 +0530

    Add files via upload

---
+++ For_Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i<=20; i = i+2)  // Table Of 2
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
